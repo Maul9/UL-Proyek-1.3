84ed304527602ecdfdf56ecd3a9fec62ac3e117a
Maul9 <62454961+Maul9@users.noreply.github.com>
2020-04-18 19:09:02 +0700

    Create Soal 3.js

---
+++ Soal 3.js
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+ 
+int baris, spasi, bintang, a;
+cout << ” masukan tinggi “;
+cin >> a;
+for (baris=1; baris<=a; baris++)
+{
+for (spasi=1; spasi<=a-baris; spasi++)
+{
+cout<<” “;
+}
+for (bintang=1; bintang<=baris; bintang++)
+{
+cout<<“* “;
+}
+cout<<endl;
+}
+ 
+return 0;
+}
